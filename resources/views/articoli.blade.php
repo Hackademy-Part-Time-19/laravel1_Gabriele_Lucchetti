5c1f6ca11b2ebc34c655f42e7d233b722475df68
GabLux <gabrielelucchetti@live.it>
2024-01-23 19:23:59 +0100

    laravel

---
+++ resources/views/articoli.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Articoli</title>
+</head>
+<body>
+    <h1>ARTICOLI</h1>
+    <a href="/home">Home</a>
+    <a href="/chisono">Chi sono</a>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Questa e' la Home del sito di Gabriele Lucchetti</h1>
+    <a href="/chisono">Chi sono</a>
+    <a href="/articoli">Articoli</a>
+</body>
+</html>
